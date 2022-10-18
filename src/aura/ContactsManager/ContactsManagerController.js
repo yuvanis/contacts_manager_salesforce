7fab50bfb8b4208d03da8d98f60d3ae95faa61c4
yuvanis <uvnistratov@gmail.com>
2022-10-18 05:55:43 +0300

    feat: add sort

--- src/aura/ContactsManager/ContactsManagerController.js
+++ src/aura/ContactsManager/ContactsManagerController.js
@@ -1,6 +1,9 @@
 ({
     init : function(component, event, helper) {
         helper.fetchContacts(component, event, helper);
+    },
+
+    handleSort: function(component, event, helper) {
+        helper.handleSort(component, event, helper);
     }
 })
-
--- src/aura/ContactsManager/ContactsManagerHelper.js
+++ src/aura/ContactsManager/ContactsManagerHelper.js
@@ -1,13 +1,13 @@
 ({
     fetchContacts : function(component, event, helper) {
         component.set('v.columns', [
-            {label: 'Name', fieldName:'Name', type: 'text'},
-            {label: 'Email', fieldName:'Email', type:'email'},
-            {label: 'Contact Level', fieldName: 'Contact_Level__c', type: 'text'},
-            {label: 'Account Name', fieldName: 'AccountName', type: 'text'},
-            {label: 'Owner Name', fieldName: 'OwnerName', type: 'text'},
-            {label: 'Created By', fieldName: 'CreatedByName', type: 'text'},
-            {label: 'Created Date', fieldName: 'CreatedDate', type: 'date',
+            {label: 'Name', fieldName:'Name', type: 'text', sortable: true},
+            {label: 'Email', fieldName:'Email', type:'email', sortable: true},
+            {label: 'Contact Level', fieldName: 'Contact_Level__c', type: 'text', sortable: true},
+            {label: 'Account Name', fieldName: 'AccountName', type: 'text', sortable: true},
+            {label: 'Owner Name', fieldName: 'OwnerName', type: 'text', sortable: true},
+            {label: 'Created By', fieldName: 'CreatedByName', type: 'text', sortable: true},
+            {label: 'Created Date', fieldName: 'CreatedDate', type: 'date', sortable: true,
                 typeAttributes: {
                     day: 'numeric',
                     month: 'short',
@@ -35,5 +35,32 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    sortBy: function(field, reverse, example) {
+        let key = example
+            ? function(x) {
+                return example(x[field]);
+            }
+            : function(x) {
+                return x[field];
+            };
+
+        return function(a, b) {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        };
+    },
+
+    handleSort: function(component, event) {
+        const sortedBy = event.getParam('fieldName');
+        const sortDirection = event.getParam('sortDirection');
+        const cloneData = component.get("v.contactsList");
+        cloneData.sort((this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1)));
+        component.set('v.contactsList', cloneData);
+        component.set('v.sortDirection', sortDirection);
+        component.set('v.sortedBy', sortedBy);
     }
+
 })
